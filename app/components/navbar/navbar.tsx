faa022ac52fa732704ac9e52272e5c11b9c1f13e
arda <arda@ardas-Mac-mini.local>
2024-03-18 00:08:08 +0300

    Update navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,9 +1,13 @@
-import "@/app/components/navbar/navbar.css"
+import "@/app/components/navbar/navbar.css";
 
-export default function Navbar(){
-    return(
-        <main>
-            <div className="navbar flex justify-center p-4 shadow-lg"><h1 className="title flex p-2"><a href="/">aquilarda</a></h1></div>
-        </main>
-    );
+export default function Navbar() {
+  return (
+    <main>
+      <div className="navbar flex justify-center p-4 shadow-lg">
+        <h1 className="title flex p-2">
+          <a href="/">aquilarda</a>
+        </h1>
+      </div>
+    </main>
+  );
 }
