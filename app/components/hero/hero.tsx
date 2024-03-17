a1d545fed229ac26dc7ac6db2a1d135991b3d1c0
arda <arda@ardas-Mac-mini.local>
2024-03-18 00:10:00 +0300

    Create and Update hero

---
+++ app/components/hero/hero.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "@/app/components/hero/hero.css";
+
+const Hero = () => {
+  return (
+    <main className="main">
+      <div className="main-div flex justify-center items-center">
+        
+        <div className="profile-image rounded-2xl m-8">
+          <img
+            src="/akame.png"
+            alt="Profile Picture"
+            className="picture-class rounded-xl shadow-2xl"
+          />
+        </div>
+
+        <div className="about-me flex flex-col items-center text-center m-8">
+          <h1 className="about-me-title">
+            Front-end Web Developer
+          </h1>
+          <h3 className="about-me-text text-center"> <br />
+              Hi! I’m Arda. A enthusiastic UI/UX designer and front-end web
+              developer.
+            </h3>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
