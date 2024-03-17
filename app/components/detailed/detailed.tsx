028fdea1df4cc298a0f3c23dd7aeb391b4aae837
arda <arda@ardas-Mac-mini.local>
2024-03-18 00:10:50 +0300

    Create and Update detailed-about-me

---
+++ app/components/detailed/detailed.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "@/app/components/detailed/detailed.css";
+
+const Detailed = () => {
+  return (
+    <main>
+      <div className="detailed-main-div flex justify-center items-center">
+        <div className="detailed-about-me flex flex-col items-center text-center m-8">
+          <h1 className="detailed-about-me-title flex-col">
+            About Me
+          </h1>
+          <h3 className="detailed-about-me-text">
+              <br />
+              A 16-years-old UI/UX designer and front-end web
+              developer from Turkey. My goal is creating nice and clean
+              websites. I mostly use React/Next.js with Tailwind CSS and
+              TypeScript. I support Free and Open Source Software.
+            </h3>
+        </div>
+
+
+        <div className="detailed-profile-image rounded-2xl m-8">
+          <img
+            src="/akame_new.png"
+            alt="Akame"
+            className="detailed-picture-class rounded-xl shadow-2xl"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Detailed;
