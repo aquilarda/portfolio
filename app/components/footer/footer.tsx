95a7669834455266ce62e73f198c2d052117863e
arda <arda@ardas-Mac-mini.local>
2024-03-18 00:07:37 +0300

    Update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,8 +3,8 @@ import "@/app/components/footer/footer.css"
 export default function Footer(){
     return(
         <main><div className="footer flex justify-evenly p-4 shadow-2xl text-center items-center">
-        <a href="/">aquilarda</a>
-        <a href="https://github.com/aquilarda"><img src="/github.png" alt="Github" width={50}/></a>
+        <a href="/" className="p-2">aquilarda</a>
+        <a href="https://github.com/aquilarda" className="p-2"><img src="/github.png" alt="Github" width={50}/></a>
         </div>
         </main>
     );
