74ae490d11471bd477aef97dba5d1ef8cf50916c
arda <arda@ardas-Mac-mini.local>
2024-03-18 00:09:06 +0300

    Create and Update contact

---
+++ app/components/contact/contact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "@/app/components/contact/contact.css";
+
+const Contact = () => {
+  return (
+    <main>
+      <div className="contact-div flex justify-center text-center items-center m-8">
+      <a href="mailto:ahmetardayesildag@icloud.com" className="shadow-2xl p-4 rounded-xl">Mail <br />
+      ahmetardayesildag@icloud.com</a>
+      </div>
+    </main>
+  );
+};
+
+export default Contact;
